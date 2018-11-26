f3065efc07135e384e0b83e6084a5e6f577385b6
Tristan <tristanw3@outlook.com>
2018-11-26 02:15:57 +0000

    second commit

--- flask_app.py
+++ flask_app.py
@@ -2,33 +2,64 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
+import pandas as pd
 
-
+df = pd.read_csv('assets/NSW_Pop_Proj.csv')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Projection Comparison'),
+app.layout = html.Div(
+    children=[
+        html.H1(
+            style={
+                'text-align':'center', 
+                'color':'rgb(33,150,243)'
+            }, 
+        
+            children='Heading'
+        ),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+        html.Div(
+            children='Second Heading'
+        ),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [2011, 2016, 2021, 2026, 2031, 2036], 'y': [7218550, 7709400, 8194450, 8633200, 9030000, 9379600], 'type': 'line', 'name': 'DPE'}
-                # {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'ABS'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+        html.Div(
+            style={
+                'display':'flex',
+                'width':'100%'
+            },
+
+            children=html.Div(
+                style={
+                    'width':'50%',
+                    'justify-content':'center'
+                },
+
+                children=dcc.Graph(
+                    id='example-graph',
+                    figure={
+                        'data': [
+                            {'x': [2011, 2016, 2021, 2026, 2031, 2036], 'y': [7218550, 7709400, 8194450, 8633200, 9030000, 9379600], 'type': 'line', 'name': 'DPE'}
+                            # {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'ABS'},
+                        ],
+                        'layout': {
+                            'title': 'Dash Data Visualization'
+                        }
+                    }
+                )
+            )
+        ),
+
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict("rows")
+        )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
